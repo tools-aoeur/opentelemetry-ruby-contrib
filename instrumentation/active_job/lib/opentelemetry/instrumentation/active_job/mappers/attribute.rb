c9ab7d6eddf3eff87712dd998270131d5dc63629
Hare Ram Rai <guru.java.ram@gmail.com>
2023-09-26 11:39:56 +0200

    fix: Add peer.service option for ActiveJob instrumentation
    fix: Rubocop

--- instrumentation/active_job/lib/opentelemetry/instrumentation/active_job/mappers/attribute.rb
+++ instrumentation/active_job/lib/opentelemetry/instrumentation/active_job/mappers/attribute.rb
@@ -31,10 +31,16 @@ module OpenTelemetry
             otel_attributes['messaging.active_job.message.provider_job_id'] = job.provider_job_id.to_s if job.provider_job_id
             # This can be problematic if programs use invalid attribute types like Symbols for priority instead of using Integers.
             otel_attributes['messaging.active_job.message.priority'] = job.priority.to_s if job.priority
+            # add peer service if exists
+            otel_attributes['peer.service'] = otel_config[:peer_service] if otel_config[:peer_service]
 
             otel_attributes.compact!
 
             otel_attributes
+          end
+
+          def otel_config
+            ActiveJob::Instrumentation.instance.config
           end
         end
       end
--- instrumentation/active_job/test/opentelemetry/instrumentation/active_job/handlers/perform_test.rb
+++ instrumentation/active_job/test/opentelemetry/instrumentation/active_job/handlers/perform_test.rb
@@ -336,4 +336,16 @@ describe OpenTelemetry::Instrumentation::ActiveJob::Handlers::Perform do
       _(process_span).wont_be_nil
     end
   end
+
+  describe 'peer.service' do
+    let(:config) { { peer_service: 'MyActiveJobService' } }
+
+    it 'add peer.service info' do
+      TestJob.perform_later
+
+      [publish_span, process_span].each do |span|
+        _(span.attributes['peer.service']).must_equal('MyActiveJobService')
+      end
+    end
+  end
 end
