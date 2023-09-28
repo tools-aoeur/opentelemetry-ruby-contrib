773f9dfcb401e4af99c0f63aefda71231b092582
Hare Ram Rai <hareram.rai@alpsalpine.com>
2023-09-28 17:29:18 +0200

    feat: Add opentelemetry instrumentation for net-ldap

--- instrumentation/net_ldap/lib/opentelemetry/instrumentation/net/ldap/instrumentation.rb
+++ instrumentation/net_ldap/lib/opentelemetry/instrumentation/net/ldap/instrumentation.rb
@@ -20,17 +20,12 @@ module OpenTelemetry
             defined?(::Net::LDAP)
           end
 
-          ## Supported configuration keys for the install config hash:
-          #
-          # untraced_hosts: if a request's address matches any of the `String`
-          #   or `Regexp` in this array, the instrumentation will not record a
-          #   `kind = :client` representing the request and will not propagate
-          #   context in the request.
-          option :untraced_hosts, default: [], validate: :array
+          option :peer_service, default: nil, validate: :string
 
           private
 
           def require_dependencies
+            require_relative 'instrumentation_service'
             require_relative 'patches/instrumentation'
           end
 
